f5c76c5e7f7eafe2b70534ba6e441f7ad03ff165
James Harris <mailjamesharris@gmail.com>
2019-01-30 19:17:11 +1000

    Add various convenience functions for relating handler types to message roles.

--- handler/type.go
+++ handler/type.go
@@ -1,5 +1,9 @@
 package handler
 
+import (
+	"github.com/dogmatiq/enginekit/message"
+)
+
 // Type is an enumeration of the types of handlers.
 type Type string
 
@@ -63,5 +67,79 @@ func (t Type) MustBe(types ...Type) {
 func (t Type) MustNotBe(types ...Type) {
 	if t.Is(types...) {
 		panic("unexpected type: " + t)
+	}
+}
+
+// IsConsumerOf returns true if handlers of type t can consume messages with the
+// given role.
+func (t Type) IsConsumerOf(r message.Role) bool {
+	return r.Is(t.Consumes()...)
+}
+
+// IsProducerOf returns true if handlers of type t can produce messages with the
+// given role.
+func (t Type) IsProducerOf(r message.Role) bool {
+	return r.Is(t.Produces()...)
+}
+
+// Consumes returns the roles of messages that can be consumed by handlers of
+// this type.
+func (t Type) Consumes() []message.Role {
+	t.MustValidate()
+
+	switch t {
+	case AggregateType:
+		return []message.Role{message.CommandRole}
+	case ProcessType:
+		return []message.Role{message.EventRole, message.TimeoutRole}
+	case IntegrationType:
+		return []message.Role{message.CommandRole}
+	default: // ProjectionType
+		return []message.Role{message.EventRole}
+	}
+}
+
+// Produces returns the roles of messages that can be produced by handlers of
+// this type.
+func (t Type) Produces() []message.Role {
+	t.MustValidate()
+
+	switch t {
+	case AggregateType:
+		return []message.Role{message.EventRole}
+	case ProcessType:
+		return []message.Role{message.CommandRole, message.TimeoutRole}
+	case IntegrationType:
+		return []message.Role{message.EventRole}
+	default: // ProjectionType
+		return nil
+	}
+}
+
+// ConsumersOf returns the handler types that can consume messages with the
+// given role.
+func ConsumersOf(r message.Role) []Type {
+	r.MustValidate()
+
+	switch r {
+	case message.CommandRole:
+		return []Type{AggregateType, IntegrationType}
+	case message.EventRole:
+		return []Type{ProcessType, ProjectionType}
+	default: // message.TimeoutRole
+		return []Type{ProcessType}
+	}
+}
+
+// ProducersOf returns the handler types that can produces messages with the
+// given role.
+func ProducersOf(r message.Role) []Type {
+	switch r {
+	case message.CommandRole:
+		return []Type{ProcessType}
+	case message.EventRole:
+		return []Type{AggregateType, IntegrationType}
+	default: // message.TimeoutRole
+		return []Type{ProcessType}
 	}
 }
--- handler/type_test.go
+++ handler/type_test.go
@@ -2,6 +2,7 @@ package handler_test
 
 import (
 	. "github.com/dogmatiq/enginekit/handler"
+	"github.com/dogmatiq/enginekit/message"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
@@ -53,6 +54,121 @@ var _ = Describe("type Type", func() {
 			Expect(func() {
 				AggregateType.MustNotBe(AggregateType, ProcessType)
 			}).To(Panic())
+		})
+	})
+
+	Describe("func IsConsumerOf", func() {
+		It("returns the expected values", func() {
+			Expect(AggregateType.IsConsumerOf(message.CommandRole)).To(BeTrue())
+			Expect(AggregateType.IsConsumerOf(message.EventRole)).To(BeFalse())
+			Expect(AggregateType.IsConsumerOf(message.TimeoutRole)).To(BeFalse())
+
+			Expect(ProcessType.IsConsumerOf(message.CommandRole)).To(BeFalse())
+			Expect(ProcessType.IsConsumerOf(message.EventRole)).To(BeTrue())
+			Expect(ProcessType.IsConsumerOf(message.TimeoutRole)).To(BeTrue())
+
+			Expect(IntegrationType.IsConsumerOf(message.CommandRole)).To(BeTrue())
+			Expect(IntegrationType.IsConsumerOf(message.EventRole)).To(BeFalse())
+			Expect(IntegrationType.IsConsumerOf(message.TimeoutRole)).To(BeFalse())
+
+			Expect(ProjectionType.IsConsumerOf(message.CommandRole)).To(BeFalse())
+			Expect(ProjectionType.IsConsumerOf(message.EventRole)).To(BeTrue())
+			Expect(ProjectionType.IsConsumerOf(message.TimeoutRole)).To(BeFalse())
+		})
+	})
+
+	Describe("func IsProducerOf", func() {
+		It("returns the expected values", func() {
+			Expect(AggregateType.IsProducerOf(message.CommandRole)).To(BeFalse())
+			Expect(AggregateType.IsProducerOf(message.EventRole)).To(BeTrue())
+			Expect(AggregateType.IsProducerOf(message.TimeoutRole)).To(BeFalse())
+
+			Expect(ProcessType.IsProducerOf(message.CommandRole)).To(BeTrue())
+			Expect(ProcessType.IsProducerOf(message.EventRole)).To(BeFalse())
+			Expect(ProcessType.IsProducerOf(message.TimeoutRole)).To(BeTrue())
+
+			Expect(IntegrationType.IsProducerOf(message.CommandRole)).To(BeFalse())
+			Expect(IntegrationType.IsProducerOf(message.EventRole)).To(BeTrue())
+			Expect(IntegrationType.IsProducerOf(message.TimeoutRole)).To(BeFalse())
+
+			Expect(ProjectionType.IsProducerOf(message.CommandRole)).To(BeFalse())
+			Expect(ProjectionType.IsProducerOf(message.EventRole)).To(BeFalse())
+			Expect(ProjectionType.IsProducerOf(message.TimeoutRole)).To(BeFalse())
+		})
+	})
+
+	Describe("func Consumes", func() {
+		It("returns the expected values", func() {
+			Expect(AggregateType.Consumes()).To(ConsistOf(
+				message.CommandRole,
+			))
+
+			Expect(ProcessType.Consumes()).To(ConsistOf(
+				message.EventRole,
+				message.TimeoutRole,
+			))
+
+			Expect(IntegrationType.Consumes()).To(ConsistOf(
+				message.CommandRole,
+			))
+
+			Expect(ProjectionType.Consumes()).To(ConsistOf(
+				message.EventRole,
+			))
+		})
+	})
+
+	Describe("func Produces", func() {
+		It("returns the expected values", func() {
+			Expect(AggregateType.Produces()).To(ConsistOf(
+				message.EventRole,
+			))
+
+			Expect(ProcessType.Produces()).To(ConsistOf(
+				message.CommandRole,
+				message.TimeoutRole,
+			))
+
+			Expect(IntegrationType.Produces()).To(ConsistOf(
+				message.EventRole,
+			))
+
+			Expect(ProjectionType.Produces()).To(BeEmpty())
+		})
+	})
+
+	Describe("func ConsumersOf", func() {
+		It("returns the expected values", func() {
+			Expect(ConsumersOf(message.CommandRole)).To(ConsistOf(
+				AggregateType,
+				IntegrationType,
+			))
+
+			Expect(ConsumersOf(message.EventRole)).To(ConsistOf(
+				ProcessType,
+				ProjectionType,
+			))
+
+			Expect(ConsumersOf(message.TimeoutRole)).To(ConsistOf(
+				ProcessType,
+			))
+		})
+	})
+
+	Describe("func ProducersOf", func() {
+		It("returns the expected values", func() {
+			Expect(ProducersOf(message.CommandRole)).To(ConsistOf(
+				ProcessType,
+			))
+
+			Expect(ProducersOf(message.EventRole)).To(ConsistOf(
+				AggregateType,
+				IntegrationType,
+			))
+
+			Expect(ProducersOf(message.TimeoutRole)).To(ConsistOf(
+				ProcessType,
+			))
 		})
 	})
 })

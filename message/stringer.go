9bbf25dfee07fb4acc0fcd4268f2d1a4a060c67c
James Harris <james.harris@icecave.com.au>
2019-02-02 13:45:06 +1000

    Add `message.ToString()`.

---
+++ message/stringer.go
@@ -0,0 +1,36 @@
+package message
+
+import (
+	"fmt"
+
+	"github.com/dogmatiq/dogma"
+)
+
+// Stringer is an interface implemented by messages that can describe themselves.
+//
+// It can be used to provide a more specific message description for message
+// types that already implement fmt.Stringer, such as when the message
+// implementations are generated Protocol Buffers structs.
+type Stringer interface {
+	// MessageString returns a human-readable description of the message.
+	MessageString() string
+}
+
+// ToString returns a string representation of m.
+//
+// If m implements Stringer, it returns m.MessageString(). Otherwise, if m
+// implements fmt.Stringer, it returns m.String().
+//
+// Finally, if m does not implement either of these interfaces, it returns
+// the standard Go "%#v" representation of the message.
+func ToString(m dogma.Message) string {
+	if s, ok := m.(Stringer); ok {
+		return s.MessageString()
+	}
+
+	if s, ok := m.(fmt.Stringer); ok {
+		return s.String()
+	}
+
+	return fmt.Sprintf("%#v", m)
+}
---
+++ message/stringer_test.go
@@ -0,0 +1,53 @@
+package message_test
+
+import (
+	. "github.com/dogmatiq/enginekit/message"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+type messageStringerMessage struct{}
+
+func (messageStringerMessage) MessageString() string {
+	return "<string>"
+}
+
+func (messageStringerMessage) String() string {
+	panic("unexpected call")
+}
+
+type fmtStringerMessage struct{}
+
+func (fmtStringerMessage) String() string {
+	return "<string>"
+}
+
+type nonStringerMessage struct {
+	Value int
+}
+
+var _ = Describe("func ToString", func() {
+	It("returns the result of MessageString() if the message implements message.Stringer", func() {
+		Expect(
+			ToString(messageStringerMessage{}),
+		).To(Equal(
+			"<string>",
+		))
+	})
+
+	It("returns the result of String() if the message implements fmt.Stringer", func() {
+		Expect(
+			ToString(fmtStringerMessage{}),
+		).To(Equal(
+			"<string>",
+		))
+	})
+
+	It("returns the standard Go representation if the message implements does not implement a Stringer interface", func() {
+		Expect(
+			ToString(nonStringerMessage{100}),
+		).To(Equal(
+			"message_test.nonStringerMessage{Value:100}",
+		))
+	})
+})

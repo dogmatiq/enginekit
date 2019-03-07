5c9e6490a6f12a7e5eaf3d65ab64749f716128f9
James Harris <mailjamesharris@gmail.com>
2019-03-07 10:03:06 +1000

    Add structs for timeouts and their meta-data.

--- message/metadata.go
+++ message/metadata.go
@@ -1,9 +1,28 @@
 package message
 
+import (
+	"time"
+
+	"github.com/dogmatiq/dogma"
+)
+
 // MetaData is a container for common message meta-data .
 type MetaData struct {
 	Correlation Correlation
 	Type        Type
 	Role        Role
 	Direction   Direction
+}
+
+// TimeoutMetaData is a container for common meta-data about a timeout message.
+type TimeoutMetaData struct {
+	ProcessName string
+	ProcessID   string
+	TimeoutTime time.Time
+}
+
+// Timeout is a container for timeout messages and their meta-data.
+type Timeout struct {
+	MetaData TimeoutMetaData
+	Message  dogma.Message
 }

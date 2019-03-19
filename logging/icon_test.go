18ab082bb9ed5831dfcf75432c1b7cbeba78c895
James Harris <mailjamesharris@gmail.com>
2019-03-19 10:01:06 +1000

    Simplify logging package for greater flexibility.

--- logging/icon_test.go
+++ logging/icon_test.go
@@ -8,7 +8,27 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-var _ = Describe("func DirectionIcon", func() {
+var _ = Describe("type Icon", func() {
+	Describe("func String()", func() {
+		It("returns the icon string", func() {
+			Expect(
+				TransactionIDIcon.String(),
+			).To(Equal("⨀"))
+		})
+	})
+})
+
+var _ = Describe("type IconWithLabel", func() {
+	Describe("func IconWithLabel()", func() {
+		It("returns the icon and label", func() {
+			Expect(
+				TransactionIDIcon.WithLabel("<foo>").String(),
+			).To(Equal("⨀ <foo>"))
+		})
+	})
+})
+
+var _ = Describe("func DirectionIcon()", func() {
 	It("returns the expected icon", func() {
 		Expect(DirectionIcon(message.InboundDirection, false)).To(Equal(InboundIcon))
 		Expect(DirectionIcon(message.OutboundDirection, false)).To(Equal(OutboundIcon))
@@ -20,7 +40,7 @@ var _ = Describe("func DirectionIcon", func() {
 	})
 })
 
-var _ = Describe("func HandlerTypeIcon", func() {
+var _ = Describe("func HandlerTypeIcon()", func() {
 	It("returns the expected icon", func() {
 		Expect(HandlerTypeIcon(handler.AggregateType)).To(Equal(AggregateIcon))
 		Expect(HandlerTypeIcon(handler.ProcessType)).To(Equal(ProcessIcon))

fef10d084e3860a85c409a0f8c8064f5f90c2ef2
James Harris <mailjamesharris@gmail.com>
2019-10-23 08:37:57 +1000

    Rename `identity.isValidName()` to `isValid()`.

--- identity/identity.go
+++ identity/identity.go
@@ -35,14 +35,14 @@ func (i Identity) IsZero() bool {
 
 // Validate returns an error if i is not a valid identity.
 func (i Identity) Validate() error {
-	if !isValidName(i.Name) {
+	if !isValid(i.Name) {
 		return fmt.Errorf(
 			"invalid name %#v, names must be non-empty, printable UTF-8 strings with no whitespace",
 			i.Name,
 		)
 	}
 
-	if !isValidName(i.Key) {
+	if !isValid(i.Key) {
 		return fmt.Errorf(
 			"invalid key %#v, keys must be non-empty, printable UTF-8 strings with no whitespace",
 			i.Key,
@@ -56,11 +56,11 @@ func (i Identity) String() string {
 	return fmt.Sprintf("%s (%s)", i.Name, i.Key)
 }
 
-// isValidName returns true if n is a valid application or handler name or key.
+// isValid returns true if n is a valid application or handler name or key.
 //
 // A valid name/key is a non-empty string consisting of Unicode printable
 // characters, except whitespace.
-func isValidName(n string) bool {
+func isValid(n string) bool {
 	if len(n) == 0 {
 		return false
 	}

d82c58e77f6b4e77de02f4934a6e7c07e8691387
James Harris <contact@jamesharris.id.au>
2024-08-18 08:19:07 +1000

    Add "type tags" for command stubs.

--- enginetest/stubs/message.go
+++ enginetest/stubs/message.go
@@ -88,3 +88,58 @@ func (t TimeoutStub[T]) Validate() error {
 	}
 	return nil
 }
+
+type (
+	// TypeA is a named type used to differentiate test messages.
+	TypeA string
+	// TypeB is a named type used to differentiate test messages.
+	TypeB string
+	// TypeC is a named type used to differentiate test messages.
+	TypeC string
+	// TypeD is a named type used to differentiate test messages.
+	TypeD string
+	// TypeE is a named type used to differentiate test messages.
+	TypeE string
+	// TypeF is a named type used to differentiate test messages.
+	TypeF string
+	// TypeG is a named type used to differentiate test messages.
+	TypeG string
+	// TypeH is a named type used to differentiate test messages.
+	TypeH string
+	// TypeI is a named type used to differentiate test messages.
+	TypeI string
+	// TypeJ is a named type used to differentiate test messages.
+	TypeJ string
+	// TypeK is a named type used to differentiate test messages.
+	TypeK string
+	// TypeL is a named type used to differentiate test messages.
+	TypeL string
+	// TypeM is a named type used to differentiate test messages.
+	TypeM string
+	// TypeN is a named type used to differentiate test messages.
+	TypeN string
+	// TypeO is a named type used to differentiate test messages.
+	TypeO string
+	// TypeP is a named type used to differentiate test messages.
+	TypeP string
+	// TypeQ is a named type used to differentiate test messages.
+	TypeQ string
+	// TypeR is a named type used to differentiate test messages.
+	TypeR string
+	// TypeS is a named type used to differentiate test messages.
+	TypeS string
+	// TypeT is a named type used to differentiate test messages.
+	TypeT string
+	// TypeU is a named type used to differentiate test messages.
+	TypeU string
+	// TypeV is a named type used to differentiate test messages.
+	TypeV string
+	// TypeW is a named type used to differentiate test messages.
+	TypeW string
+	// TypeX is a named type used to differentiate test messages.
+	TypeX string
+	// TypeY is a named type used to differentiate test messages.
+	TypeY string
+	// TypeZ is a named type used to differentiate test messages.
+	TypeZ string
+)

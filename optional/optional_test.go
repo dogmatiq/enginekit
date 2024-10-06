788ce0de5a46f0e92351a2cb0dd3bbf463ca7344
James Harris <contact@jamesharris.id.au>
2024-10-07 06:58:42 +1000

    Add `optional` package.

---
+++ optional/optional_test.go
@@ -0,0 +1,70 @@
+package optional_test
+
+import (
+	"strconv"
+	"testing"
+
+	. "github.com/dogmatiq/enginekit/optional"
+)
+
+func TestOptional_some(t *testing.T) {
+	v := Some(42)
+
+	if !v.IsPresent() {
+		t.Fatal("expected value to be present")
+	}
+
+	if x := v.Value(); x != 42 {
+		t.Fatalf("unexpected value: got %v, want 42", x)
+	}
+
+	if x, ok := v.TryValue(); !ok {
+		t.Fatal("expected value to be present")
+	} else if x != 42 {
+		t.Fatalf("unexpected value: got %v, want 42", x)
+	}
+}
+
+func TestOptional_none(t *testing.T) {
+	v := None[int]()
+
+	if v.IsPresent() {
+		t.Fatal("expected value to be absent")
+	}
+
+	func() {
+		defer func() {
+			if x := recover(); x == nil {
+				t.Fatal("expected a panic")
+			}
+		}()
+
+		v.Value()
+	}()
+
+	if x, ok := v.TryValue(); ok {
+		t.Fatal("expected value to be absent")
+	} else if x != 0 {
+		t.Fatalf("unexpected value: got %v, want 0", x)
+	}
+}
+
+func TestTransform(t *testing.T) {
+	v := Some(42)
+	u := Transform(v, strconv.Itoa)
+
+	if !u.IsPresent() {
+		t.Fatal("expected transformed value to be present")
+	}
+
+	if x := u.Value(); x != "42" {
+		t.Fatalf("unexpected transformed value: got %v, want 'x42'", x)
+	}
+
+	v = None[int]()
+	u = Transform(v, strconv.Itoa)
+
+	if u.IsPresent() {
+		t.Fatal("expected transformed value to be absent")
+	}
+}

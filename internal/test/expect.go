0bb644530d269b73a84c3b1bcb06bf6b0810417c
James Harris <contact@jamesharris.id.au>
2024-10-07 10:20:07 +1000

    Add options to `test.Expect()` to allow it to compare optional types.

--- internal/test/expect.go
+++ internal/test/expect.go
@@ -1,8 +1,11 @@
 package test
 
 import (
+	"reflect"
 	"testing"
 
+	"github.com/dogmatiq/enginekit/enginetest/stubs"
+	"github.com/dogmatiq/enginekit/message"
 	"github.com/google/go-cmp/cmp"
 	"github.com/google/go-cmp/cmp/cmpopts"
 	"google.golang.org/protobuf/testing/protocmp"
@@ -22,6 +25,17 @@ func Expect[T any](
 			protocmp.Transform(),
 			cmpopts.EquateEmpty(),
 			cmpopts.EquateErrors(),
+			cmp.Comparer(func(a, b message.Type) bool { return a == b }),
+			cmp.Comparer(func(a, b *stubs.ApplicationStub) bool { return a == b }),
+			cmp.Comparer(func(a, b *stubs.AggregateMessageHandlerStub) bool { return a == b }),
+			cmp.Comparer(func(a, b *stubs.ProcessMessageHandlerStub) bool { return a == b }),
+			cmp.Comparer(func(a, b *stubs.IntegrationMessageHandlerStub) bool { return a == b }),
+			cmp.Comparer(func(a, b *stubs.ProjectionMessageHandlerStub) bool { return a == b }),
+			cmp.Exporter(
+				func(t reflect.Type) bool {
+					return t.PkgPath() == "github.com/dogmatiq/enginekit/optional"
+				},
+			),
 		},
 		options...,
 	)

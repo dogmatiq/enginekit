8bd7eb0c8c0d4d5a9d6495ec57402e18d1c53ab9
James Harris <contact@jamesharris.id.au>
2024-10-10 07:55:05 +1000

    Add validation tests for projection handlers.

---
+++ config/handler_projection_test.go
@@ -0,0 +1,112 @@
+package config_test
+
+import (
+	"testing"
+
+	"github.com/dogmatiq/dogma"
+	. "github.com/dogmatiq/enginekit/config"
+	"github.com/dogmatiq/enginekit/config/runtimeconfig"
+	. "github.com/dogmatiq/enginekit/enginetest/stubs"
+)
+
+func TestProjection_validation(t *testing.T) {
+	cases := []struct {
+		Name    string
+		Want    string
+		Handler dogma.ProjectionMessageHandler
+	}{
+		{
+			"valid",
+			``, // no error
+			&ProjectionMessageHandlerStub{
+				ConfigureFunc: func(c dogma.ProjectionConfigurer) {
+					c.Identity("name", "19cb98d5-dd17-4daf-ae00-1b413b7b899a")
+					c.Routes(
+						dogma.HandlesEvent[EventStub[TypeA]](),
+					)
+				},
+			},
+		},
+		{
+			"nil projection",
+			`projection is invalid:` +
+				"\n" + `  - no identity is configured` +
+				"\n" + `  - expected at least one "HandlesEvent" route`,
+			nil,
+		},
+		{
+			"unconfigured projection",
+			`projection:github.com/dogmatiq/enginekit/enginetest/stubs.ProjectionMessageHandlerStub is invalid:` +
+				"\n" + `  - no identity is configured` +
+				"\n" + `  - expected at least one "HandlesEvent" route`,
+			&ProjectionMessageHandlerStub{},
+		},
+		{
+			"projection identity must be valid",
+			`projection:github.com/dogmatiq/enginekit/enginetest/stubs.ProjectionMessageHandlerStub is invalid: identity:name/non-uuid is invalid: invalid key ("non-uuid"), expected an RFC 4122/9562 UUID`,
+			&ProjectionMessageHandlerStub{
+				ConfigureFunc: func(c dogma.ProjectionConfigurer) {
+					c.Identity("name", "non-uuid")
+					c.Routes(
+						dogma.HandlesEvent[EventStub[TypeA]](),
+					)
+				},
+			},
+		},
+		{
+			"projection must not have multiple identities",
+			`projection:github.com/dogmatiq/enginekit/enginetest/stubs.ProjectionMessageHandlerStub is invalid: multiple identities are configured: identity:foo/63bd2756-2397-4cae-b33b-96e809b384d8, identity:foo/63bd2756-2397-4cae-b33b-96e809b384d8 and identity:bar/ee316cdb-894c-454e-91dd-ec0cc4531c42`,
+			&ProjectionMessageHandlerStub{
+				ConfigureFunc: func(c dogma.ProjectionConfigurer) {
+					c.Identity("foo", "63bd2756-2397-4cae-b33b-96e809b384d8")
+					c.Identity("foo", "63bd2756-2397-4cae-b33b-96e809b384d8")
+					c.Identity("bar", "ee316cdb-894c-454e-91dd-ec0cc4531c42")
+					c.Routes(
+						dogma.HandlesEvent[EventStub[TypeA]](),
+					)
+				},
+			},
+		},
+		{
+			"projection must handle at least one event type",
+			`projection:github.com/dogmatiq/enginekit/enginetest/stubs.ProjectionMessageHandlerStub is invalid: expected at least one "HandlesEvent" route`,
+			&ProjectionMessageHandlerStub{
+				ConfigureFunc: func(c dogma.ProjectionConfigurer) {
+					c.Identity("handler", "d1e04684-ec56-44a7-8c7d-f111b2d6b2d2")
+					// <-- MISSING "HandlesEvent" ROUTE
+				},
+			},
+		},
+		{
+			"projection must not have multiple routes for the same event type",
+			`projection:github.com/dogmatiq/enginekit/enginetest/stubs.ProjectionMessageHandlerStub is invalid: multiple "HandlesEvent" routes are configured for event:github.com/dogmatiq/enginekit/enginetest/stubs.EventStub[github.com/dogmatiq/enginekit/enginetest/stubs.TypeA]`,
+			&ProjectionMessageHandlerStub{
+				ConfigureFunc: func(c dogma.ProjectionConfigurer) {
+					c.Identity("handler", "d1e04684-ec56-44a7-8c7d-f111b2d6b2d2")
+					c.Routes(
+						dogma.HandlesEvent[EventStub[TypeA]](), // <-- SAME MESSAGE TYPE
+						dogma.HandlesEvent[EventStub[TypeA]](), // <-- SAME MESSAGE TYPE
+					)
+				},
+			},
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.Name, func(t *testing.T) {
+			cfg := runtimeconfig.FromProjection(c.Handler)
+
+			var got string
+			if _, err := Normalize(cfg); err != nil {
+				got = err.Error()
+			}
+
+			if c.Want != got {
+				t.Log("unexpected error:")
+				t.Log("  got:  ", got)
+				t.Log("  want: ", c.Want)
+				t.FailNow()
+			}
+		})
+	}
+}

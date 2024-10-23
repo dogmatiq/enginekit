ab058ad6cdb9eeb1e929ffdef2da20f2f70ed4a5
James Harris <contact@jamesharris.id.au>
2024-10-23 13:13:44 +1000

    Write temporary files to directory under PID.

--- config/staticconfig/analyze_test.go
+++ config/staticconfig/analyze_test.go
@@ -4,6 +4,7 @@ import (
 	"io"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
 	"testing"
 
@@ -24,10 +25,18 @@ func TestAnalyzer(t *testing.T) {
 	// Since it's under the testdata directory it is ignored by Go's tooling,
 	// but it is still subject to the same go.mod file, and hence the same
 	// version of Dogma, etc.
-	outputDir := filepath.Join(cwd, "testdata", ".aureus")
+	outputDir := filepath.Join(
+		cwd,
+		"testdata",
+		".aureus",
+		strconv.Itoa(os.Getpid()),
+	)
 	if err := os.MkdirAll(outputDir, 0700); err != nil {
 		t.Fatal(err)
 	}
+	t.Cleanup(func() {
+		os.RemoveAll(outputDir)
+	})
 
 	aureus.Run(
 		t,
@@ -103,133 +112,4 @@ func TestAnalyzer(t *testing.T) {
 			return nil
 		},
 	)
-
-	// t.Run("should parse multiple packages contain applications", func(t *testing.T) {
-	// 	apps := FromDir("testdata/multiple-apps-in-pkgs")
-
-	// 	if len(apps) != 2 {
-	// 		t.Fatalf("expected 2 applications, got %d", len(apps))
-	// 	}
-
-	// 	if expected, actual := "<app-first>",
-	// 		apps[0].Identity().Name; expected != actual {
-	// 		t.Fatalf(
-	// 			"unexpected application name: want %s, got %s",
-	// 			expected,
-	// 			actual,
-	// 		)
-	// 	}
-
-	// 	if expected, actual := "b754902b-47c8-48fc-84d2-d920c9cbdaec",
-	// 		apps[0].Identity().Key; expected != actual {
-	// 		t.Fatalf(
-	// 			"unexpected application key: want %s, got %s",
-	// 			expected,
-	// 			actual,
-	// 		)
-	// 	}
-
-	// 	if expected, actual := "<app-second>",
-	// 		apps[1].Identity().Name; expected != actual {
-	// 		t.Fatalf(
-	// 			"unexpected application name: want %s, got %s",
-	// 			expected,
-	// 			actual,
-	// 		)
-	// 	}
-
-	// 	if expected, actual := "bfaf2a16-23a0-495d-8098-051d77635822",
-	// 		apps[1].Identity().Key; expected != actual {
-	// 		t.Fatalf(
-	// 			"unexpected application key: want %s, got %s",
-	// 			expected,
-	// 			actual,
-	// 		)
-	// 	}
-	// })
-
-	// t.Run("should parse all application-level messages", func(t *testing.T) {
-	// 	apps := FromDir("testdata/app-level-messages")
-
-	// 	if len(apps) != 1 {
-	// 		t.Fatalf("expected 1 application, got %d", len(apps))
-	// 	}
-
-	// 	contains := func(
-	// 		mn message.Name,
-	// 		mk message.Kind,
-	// 		iterator iter.Seq2[message.Name, message.Kind],
-	// 	) bool {
-	// 		for k, v := range iterator {
-	// 			if k == mn && v == mk {
-	// 				return true
-	// 			}
-	// 		}
-	// 		return false
-	// 	}
-
-	// 	if !contains(
-	// 		message.NameFor[CommandStub[TypeA]](),
-	// 		message.CommandKind,
-	// 		apps[0].MessageNames().Consumed(),
-	// 	) {
-	// 		t.Fatal("expected consumed TypeA command message")
-	// 	}
-
-	// 	if !contains(
-	// 		message.NameFor[EventStub[TypeA]](),
-	// 		message.EventKind,
-	// 		apps[0].MessageNames().Consumed(),
-	// 	) {
-	// 		t.Fatal("expected consumed TypeA event message")
-	// 	}
-
-	// 	if !contains(
-	// 		message.NameFor[EventStub[TypeC]](),
-	// 		message.EventKind,
-	// 		apps[0].MessageNames().Consumed(),
-	// 	) {
-	// 		t.Fatal("expected consumed TypeC event message")
-	// 	}
-
-	// 	if !contains(
-	// 		message.NameFor[TimeoutStub[TypeA]](),
-	// 		message.TimeoutKind,
-	// 		apps[0].MessageNames().Consumed(),
-	// 	) {
-	// 		t.Fatal("expected consumed TypeA timeout message")
-	// 	}
-
-	// 	if !contains(
-	// 		message.NameFor[EventStub[TypeA]](),
-	// 		message.EventKind,
-	// 		apps[0].MessageNames().Produced(),
-	// 	) {
-	// 		t.Fatal("expected produced TypeA event message")
-	// 	}
-
-	// 	if !contains(
-	// 		message.NameFor[CommandStub[TypeB]](),
-	// 		message.CommandKind,
-	// 		apps[0].MessageNames().Produced(),
-	// 	) {
-	// 		t.Fatal("expected produced TypeB command message")
-	// 	}
-
-	// 	if !contains(
-	// 		message.NameFor[TimeoutStub[TypeA]](),
-	// 		message.TimeoutKind,
-	// 		apps[0].MessageNames().Produced(),
-	// 	) {
-	// 		t.Fatal("expected produced TypeA timeout message")
-	// 	}
-
-	// 	if !contains(
-	// 		message.NameFor[EventStub[TypeB]](),
-	// 		message.EventKind,
-	// 		apps[0].MessageNames().Produced(),
-	// 	) {
-	// 		t.Fatal("expected produced TypeB event message")
-	// 	}
-	// })
 }

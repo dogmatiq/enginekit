39ea24bf8bd3011e40af334c5bc823e333fa3a54
James Harris <contact@jamesharris.id.au>
2025-07-16 07:01:32 +1000

    Add message type ID to routes.

--- config/projection_test.go
+++ config/projection_test.go
@@ -64,7 +64,9 @@ func TestProjection(t *testing.T) {
 				Error: multiline(
 					`projection:SomeProjection is invalid:`,
 					`  - dogma.ProjectionMessageHandler value is unavailable`,
-					`  - route:handles-event:SomeEvent is invalid: message type is unavailable`,
+					`  - route:handles-event:SomeEvent is invalid:`,
+					`      - message type ID is unavailable`,
+					`      - message type is unavailable`,
 				),
 				Options: []ValidateOption{
 					ForExecution(),
@@ -324,37 +326,41 @@ func TestProjection(t *testing.T) {
 					&Route{},
 				},
 				{
-					"unexpected HandlesCommand route",
+					"unsupported HandlesCommand route",
 					`unsupported handles-command route for github.com/dogmatiq/enginekit/enginetest/stubs.CommandStub[github.com/dogmatiq/enginekit/enginetest/stubs.TypeA]`,
 					&Route{
 						RouteType:       optional.Some(HandlesCommandRouteType),
+						MessageTypeID:   optional.Some(MessageTypeID[CommandStub[TypeA]]()),
 						MessageTypeName: optional.Some("github.com/dogmatiq/enginekit/enginetest/stubs.CommandStub[github.com/dogmatiq/enginekit/enginetest/stubs.TypeA]"),
 						MessageType:     optional.Some(message.TypeFor[CommandStub[TypeA]]()),
 					},
 				},
 				{
-					"unexpected ExecutesCommand route",
+					"unsupported ExecutesCommand route",
 					`unsupported executes-command route for github.com/dogmatiq/enginekit/enginetest/stubs.CommandStub[github.com/dogmatiq/enginekit/enginetest/stubs.TypeA]`,
 					&Route{
 						RouteType:       optional.Some(ExecutesCommandRouteType),
+						MessageTypeID:   optional.Some(MessageTypeID[CommandStub[TypeA]]()),
 						MessageTypeName: optional.Some("github.com/dogmatiq/enginekit/enginetest/stubs.CommandStub[github.com/dogmatiq/enginekit/enginetest/stubs.TypeA]"),
 						MessageType:     optional.Some(message.TypeFor[CommandStub[TypeA]]()),
 					},
 				},
 				{
-					"unexpected RecordsEvent route",
+					"unsupported RecordsEvent route",
 					`unsupported records-event route for github.com/dogmatiq/enginekit/enginetest/stubs.EventStub[github.com/dogmatiq/enginekit/enginetest/stubs.TypeA]`,
 					&Route{
 						RouteType:       optional.Some(RecordsEventRouteType),
+						MessageTypeID:   optional.Some(MessageTypeID[EventStub[TypeA]]()),
 						MessageTypeName: optional.Some("github.com/dogmatiq/enginekit/enginetest/stubs.EventStub[github.com/dogmatiq/enginekit/enginetest/stubs.TypeA]"),
 						MessageType:     optional.Some(message.TypeFor[EventStub[TypeA]]()),
 					},
 				},
 				{
-					"unexpected SchedulesTimeout route",
+					"unsupported SchedulesTimeout route",
 					`unsupported schedules-timeout route for github.com/dogmatiq/enginekit/enginetest/stubs.TimeoutStub[github.com/dogmatiq/enginekit/enginetest/stubs.TypeA]`,
 					&Route{
 						RouteType:       optional.Some(SchedulesTimeoutRouteType),
+						MessageTypeID:   optional.Some(MessageTypeID[TimeoutStub[TypeA]]()),
 						MessageTypeName: optional.Some("github.com/dogmatiq/enginekit/enginetest/stubs.TimeoutStub[github.com/dogmatiq/enginekit/enginetest/stubs.TypeA]"),
 						MessageType:     optional.Some(message.TypeFor[TimeoutStub[TypeA]]()),
 					},
